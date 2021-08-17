a9fe586a563bd791ae7a95888666a47008a3d06d
syedahmmednorthsouth <syed.ahmmed@northsouth.edu>
2021-08-17 18:35:41 +0600

    2 files modified, 1 added configuring ShippingComponent to use CartService

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
+
 
 @Component({
   selector: 'app-shipping',
@@ -6,8 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent  {
+  shippingCosts = this.cartService.getShippingPrices();
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
  
 }
